8585500a408631d6d85b675e2abe79644b5e4917
EM <em.st.wk@gmail.com>
2023-08-20 01:41:55 +0900

    ゲーム終了コマンド追加

---
+++ src/main/java/plugin/treasurehunt/command/HuntEndCommand.java
@@ -0,0 +1,62 @@
+package plugin.treasurehunt.command;
+
+import java.util.List;
+import java.util.Optional;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import plugin.treasurehunt.Main;
+import plugin.treasurehunt.data.ExecutingPlayer;
+
+public class HuntEndCommand extends BaseCommand {
+
+  private final Main main;
+  private final TreasureHuntCommand treasureHuntCommand;
+
+  public HuntEndCommand(Main main, TreasureHuntCommand treasureHuntCommand) {
+    this.main = main;
+    this.treasureHuntCommand = treasureHuntCommand;
+  }
+
+
+  @Override
+  public boolean onExecutePlayerCommand(Player player, Command command, String label,
+      String[] args) {
+
+    List<ExecutingPlayer> executingPlayerList = treasureHuntCommand.getExecutingPlayerList();
+
+    boolean isExecutingPlayer = executingPlayerList.stream()
+        .filter(p -> p.getPlayerName().equals(player.getName()))
+        .anyMatch(p -> ((p.getCountdown() != null && !p.getCountdown().isCancelled())
+            || (p.getGameScheduler() != null && !p.getGameScheduler().isCancelled())));
+
+    if (executingPlayerList.isEmpty() || !isExecutingPlayer) {
+      player.sendMessage("このコマンドは宝探しゲーム実行中のみ使用できます。");
+      return false;
+    }
+
+    Optional<ExecutingPlayer> nowExecutingPlayer = executingPlayerList.stream()
+        .filter(p -> p.getPlayerName().equals(player.getName()))
+        .findFirst();
+
+    nowExecutingPlayer.ifPresent(p -> {
+      if (p.getCountdown() != null && !p.getCountdown().isCancelled()) {
+        p.getCountdown().cancel();
+      } else if (p.getGameScheduler() != null && !p.getGameScheduler().isCancelled()) {
+        p.getGameScheduler().cancel();
+        p.getGameScheduler().getBossBar().removeAll();
+      }
+
+      player.sendMessage("宝探しゲームを終了しました。");
+      executingPlayerList.remove(p);
+    });
+
+    return false;
+  }
+
+  @Override
+  public boolean onExecuteNPCCommand(CommandSender sender, Command command, String label,
+      String[] args) {
+    return false;
+  }
+}
