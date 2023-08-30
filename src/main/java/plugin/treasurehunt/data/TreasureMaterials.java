b9e862bc31b1d69e508816cb819e736125d4873c
EM <em.st.wk@gmail.com>
2023-08-30 22:13:45 +0900

    リスト内データ修正

--- src/main/java/plugin/treasurehunt/data/TreasureMaterials.java
+++ src/main/java/plugin/treasurehunt/data/TreasureMaterials.java
@@ -25,16 +25,27 @@ public class TreasureMaterials {
 
     int initGameTime = main.getConfig().getInt("game.initGameTime");
 
+    // ボーナススコアは0+10段階（easy:0~2、normal:3~6、hard:7~10）で設定しています。
     this.easyTreasureList = List.of(
-        new TreasureData(Material.DIRT, 0),
-        new TreasureData(Material.SAND, initGameTime / 4)
+        new TreasureData(Material.PUMPKIN, 0),
+        new TreasureData(Material.DANDELION, initGameTime / 10),
+        new TreasureData(Material.WHITE_WOOL, initGameTime / 10),
+        new TreasureData(Material.BEEF, (initGameTime / 10) * 2),
+        new TreasureData(Material.PORKCHOP, (initGameTime / 10) * 2)
     );
     this.normalTreasureList = List.of(
-        new TreasureData(Material.OAK_LOG, (initGameTime / 4) * 2),
-        new TreasureData(Material.DARK_OAK_LOG, (initGameTime / 4) * 3)
+        new TreasureData(Material.BONE, (initGameTime / 10) * 3),
+        new TreasureData(Material.INK_SAC, (initGameTime / 10) * 4),
+        new TreasureData(Material.LILY_OF_THE_VALLEY, (initGameTime / 10) * 5),
+        new TreasureData(Material.LAPIS_LAZULI, (initGameTime / 10) * 6),
+        new TreasureData(Material.RAW_GOLD, (initGameTime / 10) * 6)
     );
     this.hardTreasureList = List.of(
-        new TreasureData(Material.DIAMOND, initGameTime)
+        new TreasureData(Material.EMERALD, (initGameTime / 10) * 7),
+        new TreasureData(Material.DIAMOND, (initGameTime / 10) * 8),
+        new TreasureData(Material.ENDER_PEARL, (initGameTime / 10) * 9),
+        new TreasureData(Material.ANCIENT_DEBRIS, (initGameTime / 10) * 9),
+        new TreasureData(Material.WITHER_ROSE, initGameTime)
     );
   }
 
